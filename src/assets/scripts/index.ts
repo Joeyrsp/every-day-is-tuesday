6adccbfd519096f3ea1240df0502f740caa35fbe
Joeyrsp <joeysuttonpreece@gmail.com>
2020-10-23 04:08:16 +1100

    added raw hyperapp

--- src/assets/scripts/index.ts
+++ src/assets/scripts/index.ts
@@ -1,6 +1,7 @@
-import { tuesdayMachine } from "./stateMachine";
+import { tuesdayMachine } from './stateMachine';
+import { startApp } from "./app";
 
-window.addEventListener('load', function () {
+window.addEventListener('load', () => {
     /************************************** */
     /* Service worker
     /************************************** */
@@ -13,65 +14,6 @@ window.addEventListener('load', function () {
     //         console.log('ServiceWorker registration failed: ', err);
     //     });   
     // }
-
-    /************************************** */
-    /* Cookie message options
-    /************************************** */
-
-    // window.cookieconsent.initialise({
-    //     'palette': {
-    //         'popup': {
-    //         'background': '#000000'
-    //         },
-    //         'button': {
-    //         'background': '#0000ff'
-    //         }
-    //     },
-    //     'position': 'top',
-    //     'static': true,
-    //     'theme': 'classic',
-    //     'content': {
-    //         'href': ''
-    //     }
-    // })
 });
 
-let water: HTMLCanvasElement = document.querySelector('.water')
-
-water.width = window.innerWidth;
-water.height = window.innerHeight;
-
-let ctx = water.getContext('2d');
-
-ctx.fillStyle = 'white';
-ctx.strokeStyle = 'black';
-
-let total = 0
-
-let timePrev = 0;
-
-const frame = (time) => {
-    let delta = time - timePrev;
-
-    let widthOld = window.innerWidth;
-    let heightOld = window.innerHeight;
-    if (widthOld != water.width || heightOld != water.height) {
-        water.width = window.innerWidth;
-        water.height = window.innerHeight;
-        ctx.fillStyle = 'white';
-    }
-
-    ctx.fillRect(0, 0, water.width, water.height);
-
-    ctx.beginPath();
-    ctx.moveTo(total, 0);
-    ctx.lineTo(total, water.height);
-    ctx.stroke();
-
-    total = (total + delta) % water.width;
-
-    timePrev = time;
-    window.requestAnimationFrame(frame);
-}
-
-window.requestAnimationFrame(frame);
+let app = startApp();
