45fe6958b3b9d4b262e93ac80354e14b207c3536
Joeyrsp <joeysuttonpreece@gmail.com>
2020-10-27 20:17:42 +1100

    ritual text generator

--- src/init.ts
+++ src/init.ts
@@ -1,2 +1,14 @@
+import { tuesdayMachine } from "/stateMachine";
+
 // Initial app state
-export default ({ })
+export default {
+  customs: JSON.parse(window.localStorage.getItem('customs')) || [
+    'change your sheets',
+    'take a nap',
+    'stretch',
+    'write a list',
+    'wash your face',
+    'apply lotion',
+  ], // this is a demo hack. remove when appropriate
+  machine: tuesdayMachine.getInitialState('navigation.ascended.home').value,
+}
