6f5482896bba202d9bc4ae93f2125bf82137afa5
Joeyrsp <joeysuttonpreece@gmail.com>
2020-10-27 20:14:55 +1100

    componentify svgs

--- src/views/svgs/back.jsx
+++
@@ -1,7 +0,0 @@
-import { mergeClasses } from "/utils";
-
-export default (props) => mergeClasses(props,
-<svg class="primary" viewBox="0 0 17.38 30" fill="#2B3464">
-  <path d="M16.55 4.6c2.47-2.46-1.23-6.26-3.7-3.8L.75 13.46a2.64 2.64 0 000 3.7l12.1 12.03c2.47 2.46 6.17-1.23 3.7-3.81l-10.2-9.96z"/>
-</svg>
-)
--- src/views/svgs/hamburger.jsx
+++ src/views/svgs/hamburger.jsx
@@ -2,8 +2,8 @@ import { mergeClasses } from "/utils";
 
 export default (props) => mergeClasses(props,
 <svg viewBox="0 0 50 30" class="primary" fill="#2B3464">
-  <rect transform="translate(0 0)" width="50" height="8" rx="4"/>
-  <rect transform="translate(0 11)" width="50" height="8" rx="4"/>
-  <rect transform="translate(0 22)" width="50" height="8" rx="4"/>
+  <rect width="50" height="7" rx="4"/>
+  <rect y="11.5" width="50" height="7" rx="4"/>
+  <rect y="23" width="50" height="7" rx="4"/>
 </svg>
 )
